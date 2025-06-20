88e067c5dcbcc4a4a155e4e525347943e8c23a89
tejasri <venkatatejasribathini@gmail.com>
2025-06-20 13:11:27 +0530

    intermediate push

---
+++ BACKEND/interviewease/src/main/java/com/interviewease/interviewease/IntervieweaseApplication.java
@@ -0,0 +1,13 @@
+package com.interviewease.interviewease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntervieweaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntervieweaseApplication.class, args);
+	}
+
+}
